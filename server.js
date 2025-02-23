a36fecd441b79bd894e634463d76d74da07f33d2
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-02-22 22:48:23 -0500

    build: add cookie-parser and cors dependency

--- server.js
+++ server.js
@@ -4,10 +4,17 @@ const path = require("path");
 const PORT = process.env.PORT || 3500;
 const { logger } = require("./middleware/logger");
 const { errorHandler } = require("./middleware/errorHandler");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const corsOptions = require("./config/corsOptions");
 
 app.use(logger);
 
+app.use(cors(corsOptions));
+
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.use("/", express.static(path.join(__dirname, "public")));
 
