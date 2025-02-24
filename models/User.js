f9f4042ef1eb63f3ee85b2cb76557a2e86e0f2dc
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2025-02-24 03:42:34 -0500

    feat: add timestamps to user objects in DB

--- models/User.js
+++ models/User.js
@@ -1,26 +1,31 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-	username: {
-		type: String,
-		required: true,
-		unique: true,
-	},
-	password: {
-		type: String,
-		required: true,
-	},
-	roles: [
-		{
+const userSchema = new mongoose.Schema(
+	{
+		username: {
 			type: String,
 			required: true,
-			default: "Employee",
+			unique: true,
 		},
-	],
-	active: {
-		type: Boolean,
-		default: true,
+		password: {
+			type: String,
+			required: true,
+		},
+		roles: [
+			{
+				type: String,
+				required: true,
+				default: "Employee",
+			},
+		],
+		active: {
+			type: Boolean,
+			default: true,
+		},
 	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 module.exports = mongoose.model("User", userSchema);
